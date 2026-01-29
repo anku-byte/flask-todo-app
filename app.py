a57693dfb6dfcabb9eaf40ab1374cee5d06e36f5
ankita naik <ankita.naik922123@gmail.com>
2026-01-29 19:34:24 +0530

    Updated Flask project

--- app.py
+++ app.py
@@ -61,4 +61,8 @@ def update(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=10000)
+
