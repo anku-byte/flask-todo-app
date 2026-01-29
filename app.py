d2f72a7ead3d486d320fac40b7e7f5077f4a49da
ankita naik <ankita.naik922123@gmail.com>
2026-01-29 20:00:23 +0530

    fix error

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     if __name__ == "__main__":
-    with app.app_context():
+     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=10000)
 
